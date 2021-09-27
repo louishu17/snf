7e9fc71ceb2304411d3ad5870560c29630ba9e3f
louishu17 <louis.hu@duke.edu>
2021-09-26 21:56:04 -0400

    inserted boundary matricies

--- test.py
+++ test.py
@@ -1,12 +1,26 @@
 import numpy as np
 from algorithm import SmithNormalForm
-bm =  np.array([[0,0,0,0,0],
+d2 =  np.array([[0,0,0,0,0],
                 [0,0,0,0,0],
-                [],
-                [0, 0, 0, 1, 0, 1, 1],
-                [0, 0, 0, 0, 1, 0, 1]])
+                [0,0,0,1,1],
+                [0,0,1,0,1],
+                [0,1,0,0,1],
+                [1,0,0,0,1],
+                [1,1,1,0,0],
+                [1,1,0,0,0],
+                [0,1,0,1,0],
+                [0,0,1,1,0]])
+
+d1 = np.array([[1,1,0,0,0,0,0,0,0,0],
+              [1,0,1,1,0,0,0,0,0,1],
+              [0,1,1,0,1,0,0,0,1,0],
+              [0,0,0,1,0,1,1,0,0,0],
+              [0,0,0,1,0,1,1,0,0,0],
+              [0,0,0,0,0,0,1,1,1,1]])
 
 
-smithers = SmithNormalForm()
-bm_snf = smithers.smithify(bm)
-print(bm_snf)
+smith = SmithNormalForm()
+d2_snf = smith.smithify(d2)
+d1_snf = smith.smithify(d1)
+print(d2_snf)
+print(d1_snf)
